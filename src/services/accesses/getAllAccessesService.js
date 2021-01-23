2ec129f21ecb0a05816b0ce4906d5f2bd9e1e681
Muhammad Zuhri <syaifuddin.zuhri@prakerja.go.id>
2021-01-23 17:35:57 +0700

    implement pagination in get all accesses

--- src/services/accesses/getAllAccessesService.js
+++ src/services/accesses/getAllAccessesService.js
@@ -1,8 +1,27 @@
 const { Access } = require('../../models')
+const { getOpByOperator } = require('../../utils/queryHelper')
+
+
+const generateWhere = (filter = {}) => {
+  const where = {}
+  if (filter.name) {
+    where.name = {
+      [getOpByOperator(filter.name.operator)]: filter.name.value
+    }
+  }
+  if (filter.level) {
+    where.level = {
+      [getOpByOperator(filter.level.operator)]: filter.level.value
+    }
+  }
+  return where
+}
 
 module.exports = async (params) => {
-  console.log(params)
+  const where = generateWhere(params.filter)
   const result = await Access.paginate({
+    where,
+    order: params.sort,
     page: params.page.number,
     paginate: params.page.size
   })
