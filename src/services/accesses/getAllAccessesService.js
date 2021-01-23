19b15d63235958a5a94f83a3e44c10c8c292670e
Muhammad Zuhri <syaifuddin.zuhri@prakerja.go.id>
2021-01-23 15:42:53 +0700

    add pagination

--- src/services/accesses/getAllAccessesService.js
+++ src/services/accesses/getAllAccessesService.js
@@ -1,5 +1,18 @@
 const { Access } = require('../../models')
 
 module.exports = async (params) => {
-  return Access.findAll()
+  console.log(params)
+  const result = await Access.paginate({
+    page: params.page.number,
+    paginate: params.page.size
+  })
+  return {
+    data: result.docs,
+    pagination: {
+      number: params.page.number,
+      size: params.page.size,
+      pages: result.pages,
+      total: result.total
+    }
+  }
 }
