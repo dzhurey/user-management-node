c153b89e146b1fa1cbffe752d7637ed125757efa
Muhammad Zuhri <syaifuddin.zuhri@prakerja.go.id>
2021-02-08 02:27:16 +0700

    update groups to have tenant_id

--- src/models/group.js
+++ src/models/group.js
@@ -20,6 +20,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     },
+    tenantId: {
+      type: DataTypes.BIGINT,
+      allowNull: false
+    },
     description: {
       type: DataTypes.TEXT,
       defaultValue: ''
